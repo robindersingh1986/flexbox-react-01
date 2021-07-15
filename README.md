f0348baefcb8fc54c801987184768b8b34671ae5
robindersingh1986 <robindersingh1986@gmail.com>
2021-07-15 23:31:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fg5b4h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fg5b4h)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <section>Left Panel </section>
+      <section className="content">
+        Right Panel <br />
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </section>
+    </div>
+  );
+}
