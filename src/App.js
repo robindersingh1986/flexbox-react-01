53d9725fb39784610a8d977192afe1bbcf6eb2ae
robindersingh1986 <robindersingh1986@gmail.com>
2021-07-16 00:58:05 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import './style.css';
 export default function App() {
   return (
     <div className="container">
-      <section>Left Panel </section>
+      <section className="sidebar">Left Panel </section>
+
       <section className="content">
-        Right Panel <br />
-        <h1>Hello StackBlitz!</h1>
-        <p>Start editing to see some magic happen :)</p>
+        <div className="content-title">
+          Right Panel <br />
+          <h1>Hello StackBlitz!</h1>
+          <p>Start editing to see some magic happen :)</p>
+        </div>
+
+        <div className="card">
+          <div>I am a card 1</div>
+          <div>Loreum ipsum, bla bla data</div>
+        </div>
+        <div className="card">I am a card 2</div>
+        <div className="card">I am a card 3</div>
+        <div className="card">I am a card 4</div>
+        <div className="card">I am a card 5</div>
       </section>
     </div>
   );
